8c352944189ddfebbcdf53c05b53a52573274e4e
Ryan Holmes <ryan.holmes@bydeluxe.com>
2020-07-04 14:37:28 -0700

    implement AdminLTE theme

--- leave-management/Controllers/HomeController.cs
+++ leave-management/Controllers/HomeController.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using leave_management.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using leave_management.Models;
+using System.Diagnostics;
 
 namespace leave_management.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
